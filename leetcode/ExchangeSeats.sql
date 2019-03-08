6f60eba64ffc35eaa2ba83384b49a973088158b4
Tstxxy <31032518+HaominYuan@users.noreply.github.com>
2019-03-08 15:31:12 +0800

    Create ExchangeSeats.sql

---
+++ leetcode/ExchangeSeats.sql
@@ -0,0 +1,7 @@
+select (
+    case
+        when mod(id, 2) = 1 and id = (select count(*) from seat) then id
+        when mod(id, 2) = 1 then id + 1
+        else id - 1
+    end
+) as id, student from seat order by id;
